d62e3f7605e684b3e56819f055c415a493def2b6
sasha <a.krupko58@gmail.com>
2024-04-17 11:14:08 +0300

    fix: weather controller replace post with get

--- src/main/java/weather/controller/WeatherController.java
+++ src/main/java/weather/controller/WeatherController.java
@@ -1,24 +1,26 @@
 package weather.controller;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import weather.service.WeatherModel;
 import weather.service.WeatherService;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class WeatherController {
 
     private final WeatherService weatherService;
-    private final WeatherRequestMapper weatherRequestMapper;
     private final WeatherResponseMapper weatherResponseMapper;
 
-    @PostMapping("/weather")
-    public WeatherResponse.Read create(@NonNull @RequestBody WeatherRequest.Create createWeather) {
+    @GetMapping("/weather/{city}")
+    public ResponseEntity<WeatherResponse.Read> getByCity(@PathVariable String city) {
 
-        WeatherModel.Create createModel = weatherRequestMapper.toCreateModel(createWeather);
-        WeatherModel.Read readModel = weatherService.create(createModel);
-        return weatherResponseMapper.toReadResponse(readModel);
+        Optional<WeatherModel.Read> optionalReadModel = weatherService.getByCity(city);
+        return optionalReadModel
+                .map(it -> ResponseEntity.ok(weatherResponseMapper.toReadResponse(it)))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 }
