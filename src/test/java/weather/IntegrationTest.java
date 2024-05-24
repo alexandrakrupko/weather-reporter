bed4fc81399e7cbf1294523fbff4f1e07fa95467
sasha <a.krupko58@gmail.com>
2024-05-24 16:25:55 +0300

    fix: @DirtiesContext added for test containers

--- src/test/java/weather/IntegrationTest.java
+++ src/test/java/weather/IntegrationTest.java
@@ -1,6 +1,7 @@
 package weather;
 
 import com.redis.testcontainers.RedisContainer;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -9,6 +10,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
+@DirtiesContext
 public abstract class IntegrationTest {
 
     @SuppressWarnings("resource")
